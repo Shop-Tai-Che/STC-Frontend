39524e9254caf9ee2d0f740b46a4d818cd3f84ab
TanKaka123 <80939836+TanKaka123@users.noreply.github.com>
2023-10-31 20:55:00 +0700

    Revert "Add Initial Project"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,50 +4,39 @@ import { useRecoilValue } from "recoil";
 import { userState } from "../state";
 import { useAppSelector, useAppDispatch } from "../redux/hooks";
 import { searchFilterChange, statusFilterChange } from "../redux/FilterSlice";
-import { Suspense } from "react";
+import UserCard from "../components/user-card";
 import BannerImage from "../assets/images/banner.png";
-import SearchProduct from "../components/common/SearchProduct";
-import { Divider } from "../components/common/DriverDividion"; 
-import { RootState } from "../redux/store"; 
-
 
 const HomePage: React.FunctionComponent = () => {
-  const incrementAmountValue ="11111111111"
-  useAppDispatch(searchFilterChange(incrementAmountValue));
-  const count = useAppSelector((state:RootState) => state.filters);
-  console.log(count)
-  const categories =[
-    {
-      id:1,
-      name:"dasdas"
-    },
-    {
-      id:2,
-      name:"dasdas"
-    },
-    {
-      id:2,
-      name:"dasdas"
-    },
-    
-  ]
-  return (
-    <Page className="relative flex-1 flex flex-col bg-white">
-      <Box className="flex-1 overflow-auto">
-        <Box className="bg-amber-700 m-0  ">
-          <img className="w-screen h-auto " src={BannerImage} />
-        </Box>
+  const count = useAppSelector((state) => state);
+  console.log(count);
+  const assdas: string = "asdasdasdas";
+  const dispatch = useAppDispatch();
 
-        <SearchProduct />
-        <Suspense>
-        <Box className="bg-white grid grid-cols-2 gap-4 p-4">
-            
-          </Box>
-        </Suspense>
-        <Divider/>
-        <Box mt={6}>
-          <Text.Title size="small">Được yêu thích</Text.Title>
-        </Box>
+  const user = useRecoilValue(userState);
+  const navigate = useNavigate();
+
+  return (
+    <Page className="page">
+      <img className="w-screen   h-auto" src={BannerImage} />
+           {" "}
+      <Input.Search
+        label="Label"
+        helperText="Helper text"
+        placeholder="Placeholder"
+        defaultValue="Filled"
+        clearable
+        onSearch={(value) => {
+          console.log(value);
+        }}
+      />
+           {" "}
+      <Box mt={6}>
+                <Text.Title size="small">Được yêu thích</Text.Title>     {" "}
+      </Box>
+           {" "}
+      <Box mt={6}>
+                <Text.Title size="small">Được yêu thích</Text.Title>     {" "}
       </Box>
     </Page>
   );
--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { App, ZMPRouter, AnimationRoutes, SnackbarProvider, Box } from "zmp-ui";
+import { Route } from "react-router-dom";
+import { App, ZMPRouter, AnimationRoutes, SnackbarProvider } from "zmp-ui";
 import Store from "../redux/store";
 import { Provider } from "react-redux";
 import BottomNavigationComponent from "../components/layout/bottom-navigation";
@@ -10,17 +10,19 @@ import HomePage from "../pages";
 
 const MyApp = () => { 
   return (
-    <Provider store={Store}> 
-        <Box className="flex-1 flex flex-col overflow-hidden">
+    <Provider store={Store}>
+      <RecoilRoot>
+        <App>
           <SnackbarProvider>
             <ZMPRouter>
-              <AnimationRoutes> 
-                <Route path="/" element={<HomePage></HomePage>}></Route> 
+              <AnimationRoutes>
+                <Route path="/" element={<HomePage></HomePage>}></Route>
               </AnimationRoutes>
               <BottomNavigationComponent />
             </ZMPRouter>
           </SnackbarProvider>
-        </Box> 
+        </App>
+      </RecoilRoot>
     </Provider>
   );
 };
