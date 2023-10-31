7cc4d74a9edfef3fe4dc75801db442ef0ed5035c
TanKaka123 <hongtan1422002@gmail.com>
2023-10-31 22:42:39 +0700

    ✨ feat(BannerHome.tsx): add BannerHome component to display a banner image on the home page ✨ feat(bottom-navigation.tsx): add BottomNavigationComponent to display a bottom navigation bar on the page ✨ feat(tmn_search/index.tsx): add TmnSearch component to display a search input box ✨ feat(HomePage.tsx): add BannerHome and SearchProduct components to the home page ✨ feat(index.tsx): add BottomNavigationComponent to the main route to display a bottom navigation bar on all pages

---
+++ src/components/common/BannerHome.tsx
@@ -0,0 +1,13 @@
+import BannerImage from "../../assets/images/banner.png";
+import { Box } from "zmp-ui";
+import React from "react";
+
+const BannerHome: React.FunctionComponent = () => {
+  return (
+    <Box className="bg-amber-700 m-0">
+      <img className="w-screen h-auto" src={BannerImage} alt="Banner Image" />
+    </Box>
+  );
+};
+
+export default BannerHome;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,17 @@
-import React, { FC } from "react";
-import { Page } from "zmp-ui";
+import React from "react";
+import { Page, Box } from "zmp-ui";
+import BannerHome from "../components/common/BannerHome";
+import SearchProduct from "../components/services/tmn_search";
 
-const HomePage:React.FC =()=>{
-    return(
-        <Page>
-
-        </Page>
-    )
-}
+const HomePage: React.FC = () => {
+  return (
+    <Page className="relative flex-1 flex flex-col bg-white">
+      <Box className="flex-1 overflow-auto">
+        <BannerHome />
+        <SearchProduct />
+      </Box>
+    </Page>
+  );
+};
 
 export default HomePage;
