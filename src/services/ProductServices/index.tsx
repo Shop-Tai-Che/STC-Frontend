da53bcdfa1ec1ce14866ab3aba6ef940e59c2ec0
TanKaka123 <hongtan1422002@gmail.com>
2023-11-12 12:30:11 +0700

    init project

--- src/services/ProductServices/index.tsx
+++
@@ -1,3 +0,0 @@
-import { GetAllLatestProduct } from "./GetAllLatestProducts";
-
-export { GetAllLatestProduct };
--- src/utils/type/FetchState.tsx
+++
@@ -1,6 +0,0 @@
-export enum FetchState {
-  DEFAULT = "DEFAULT",
-  LOADING = "LOADING",
-  SUCCESS = "SUCCESS",
-  ERROR = "ERROR",
-}
