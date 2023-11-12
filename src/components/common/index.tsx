25bfdc38619b45eaeabe0d4c8e99d3c2c565298d
TanKaka123 <hongtan1422002@gmail.com>
2023-11-12 13:18:13 +0700

    🆕 feat(OverrideSwipe.css): add custom styles to override default styles of swiper pagination 🆕 feat(BannerHome.tsx): create a new component for displaying a banner image on the home page 🆕 feat(DividerSpace.tsx): create a new component for displaying a divider with customizable size and styling 🆕 feat(SectionText.tsx): create a new component for displaying a section title with optional padding and children 🆕 feat(SkeletonsList.tsx): create a new set of components for displaying skeleton loading placeholders 🆕 feat(index.tsx): export all common components and skeletons from the common folder 🆕 feat(FinalPriceDisplay.tsx): create a new component for displaying the final price of a product with selected options 🆕 feat(PriceDisplay.tsx): create a new component for displaying a price with currency symbol 🆕 feat(index.tsx): export all display components from the display folder 🆕 feat(ProductItem.tsx): create a new component for displaying a single product item 🆕 feat(ProductListGrid.tsx): create a new component for displaying a grid of product items 🆕 feat(ProductListSwipe.tsx): create a new component for displaying a swipeable list of product items 🆕 feat(index.tsx): export all product components from the product folder 🆕 feat(index.tsx): export the search component from the search folder 🆕 feat(HomePage.tsx): add the new components to the home page 🆕 feat(GetAllLatestProducts.tsx): create a new service for fetching the latest products 🆕 feat(index.tsx): export the product services from the services folder

---
+++ src/components/common/index.tsx
@@ -0,0 +1,6 @@
+import BannerHome from "./BannerHome";
+import DividerSpace from "./DividerSpace";
+import SectionText from "./SectionText";
+
+export * from "./SkeletonsList";
+export { BannerHome, DividerSpace, SectionText };
