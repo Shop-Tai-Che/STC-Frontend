5949a6f6bc84e744ab2cbfa90598b099f68fbdf8
kenpham4real <khanhphd.robert@gmail.com>
2024-01-01 16:47:45 +0700

    Add icon for entrypoint Seller Registration in Tab Me

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -2,5 +2,6 @@ import ReplySvg from "./reply";
 import PaymentSvg from "./payment";
 import ShipSvg from "./ship";
 import PackageSvg from "./package";
+import BusinessSvg from "./business";
 
-export { ReplySvg, PaymentSvg, ShipSvg, PackageSvg };
+export { ReplySvg, PaymentSvg, ShipSvg, PackageSvg, BusinessSvg };
