80ba6a0622021df2c918c45d8ca7a5a8ea6215fd
kenpham4real <khanhphd.robert@gmail.com>
2024-01-14 09:33:15 +0700

    Update message content in openChatScreen function

--- src/utils/helper/openchat.tsx
+++ src/utils/helper/openchat.tsx
@@ -10,13 +10,13 @@ export const openChatScreen = async (chatType, product?) => {
   let messageContent;
   switch (chatType) {
     case CHAT_TYPE.SUPPORT:
-      messageContent = `- Tên: ${product.title}\n- Link sản phẩm: https://zalo.me/s/4341737245452906396/product-detail/${product.id}\nTôi cần hỗ trợ tư vấn mua hàng với sản phẩm trên`;
+      messageContent = `- Tên: ${product.title}\n- Link sản phẩm: https://zalo.me/s/4341737245452906396/product-detail/${product.id}\nMình cần hỗ trợ tư vấn mua hàng với sản phẩm trên`;
       break;
     case CHAT_TYPE.SELLER_REGISTRATION:
-      messageContent = "Tôi muốn đăng kí bán hàng trên Shop Tái Chế";
+      messageContent = "Mình muốn đăng kí bán hàng trên Shop Tái Chế";
       break;
     case CHAT_TYPE.POST_PRODUCT:
-      messageContent = "Tôi muốn thêm sản phẩm mới trên Shop Tái Chế của mình";
+      messageContent = "Shop mình muốn thêm sản phẩm mới trên Shop Tái Chế";
       break;
   }
   try {
